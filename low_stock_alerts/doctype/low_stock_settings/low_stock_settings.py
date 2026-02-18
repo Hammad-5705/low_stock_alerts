e44f58ced07c412471fe92a5c90324532bbb87a6
Muhammad Hammad Nadeem <hamadnad2014@gmail.com>
2026-02-18 08:43:28 +0000

    removed trash

--- low_stock_alerts/doctype/low_stock_settings/low_stock_settings.py
+++
@@ -1,9 +0,0 @@
-import frappe  
-  
-def validate(doc):  
-    if doc.is_active and not doc.recipient_email:  
-        frappe.throw("Recipient Email is required when Active")  
-  
-def on_update(doc):  
-    if doc.is_active and not doc.warehouses:  
-        frappe.throw("At least one warehouse must be added when Active")
